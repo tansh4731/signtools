229a95e14340a26f5ce65e163ae82430e1200778
tanshuai <tanshuai@xdja.com>
2018-08-17 17:49:11 +0800

    add

---
+++ app.js
@@ -0,0 +1,11 @@
+//app.js
+const defaultTime = {
+  defaultWorkTime: 25,
+  defaultRestTime: 5
+}
+
+App({
+  onLaunch: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,71 @@
+const util = require('../../utils/util.js')
+
+Page({
+
+  data: {
+    currentTimeText: new util.formatTime(Date.now(), 'Y/M/D\nh:m:s'),
+    lastRecord: {},
+    toastHidden: true
+  },
+
+  onShow: function () {
+
+    let records = wx.getStorageSync('records');
+    if(records.length > 0)
+    {
+      records[0].signTime = util.formatTime(records[0].signTime, 'Y/M/D h:m:s');
+      this.setData({
+        currentTimeText: util.formatTime(Date.now(), 'Y/M/D\nh:m:s'),
+        lastRecord: records[0]
+      })
+    }
+    else
+    {
+      this.setData({
+        currentTimeText: util.formatTime(Date.now(), 'Y/M/D\nh:m:s')
+      })
+
+    }
+    
+    setInterval((function () {
+      this.updateClockTime()
+    }).bind(this), 1000)
+  },
+
+  startSign: function (e) {
+
+    var tmpLastRecord = {
+      signTime: Date.now()
+    }
+
+    this.saveRecord(tmpLastRecord)
+
+    this.setData({
+      toastHidden: false
+    })
+
+    tmpLastRecord.signTime = util.formatTime(tmpLastRecord.signTime, 'Y/M/D h:m:s');
+    this.setData({
+      lastRecord: tmpLastRecord
+    })
+
+  },
+
+  updateClockTime: function () {
+    this.setData({
+      currentTimeText: util.formatTime(Date.now(), 'Y/M/D\nh:m:s')
+    })
+  },
+
+  hideToast: function () {
+    this.setData({
+      toastHidden: true
+    })
+  },
+  
+  saveRecord: function (record) {
+    var records = wx.getStorageSync('records') || []
+    records.unshift(record)
+    wx.setStorageSync('records', records)
+  }
+})
